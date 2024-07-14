66ec8f2d47382352fa6df523db5ae4b6ccc4dceb
muhammaddaffafauzan <mobileasia215@gmail.com>
2024-07-15 04:11:25 +0700

    update: varible enviroment

--- src/server.js
+++ src/server.js
@@ -1,32 +1,29 @@
-import express from 'express';
-import cors from 'cors';
-import helmet from 'helmet';
-import fileUpload from 'express-fileupload';
-import dotenv from 'dotenv';
-import { prisma } from './database/config/config.js';
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import fileUpload from "express-fileupload";
+import dotenv from "dotenv";
+import { prisma } from "./database/config/config.js";
 
 dotenv.config();
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(helmet());
-app.use(express.json());  
-app.use(fileUpload());    
+app.use(express.json());
+app.use(fileUpload());
 
-// Contoh routing
-app.get('/hello', (req, res) => {
-  res.send('Hello, World!');
+app.get("/hello", (req, res) => {
+  res.send("Hello, World!");
 });
 
-// Menangani error middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Something broke!');
+  res.status(500).send("Something broke!");
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
